ed4c7da4a151d24d39f4862165d6293d073b73f4
Trapp <nikolastrapp@gmail.com>
2025-03-26 23:31:17 -0300

    Criando classe de configuracao e beans para DataSource com HikariCP e Flyway.

---
+++ src/main/java/br/com/trapp/cadastroagendabackend/config/database/DatabaseConfig.java
@@ -0,0 +1,60 @@
+package br.com.trapp.cadastroagendabackend.config.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableJpaAuditing
+@RequiredArgsConstructor
+public class DatabaseConfig {
+
+    private final CadastroAgendaProperties properties;
+
+    @Bean
+    DataSource dataSource() {
+        var props = properties.getDatabase();
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(props.getUrl());
+        hikariConfig.setUsername(props.getUsername());
+        hikariConfig.setPassword(props.getPassword());
+        hikariConfig.setDriverClassName(props.getDriverClassName());
+
+        hikariConfig.setMaximumPoolSize(props.getMaximumPoolSize());
+        hikariConfig.setMinimumIdle(props.getMinimumIdle());
+        hikariConfig.setConnectionTimeout(props.getConnectionTimeout());
+        hikariConfig.setIdleTimeout(props.getIdleTimeout());
+        hikariConfig.setMaxLifetime(props.getMaxLifetime());
+
+        return new HikariDataSource(hikariConfig);
+    }
+
+    @Bean
+    Flyway flyway(DataSource dataSource) {
+        var props = properties.getDatabase();
+        var flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations(props.getFlywayLocations())
+                .baselineOnMigrate(true)
+                .load();
+
+        if (props.isFlywayEnabled()) {
+            flyway.migrate();
+        }
+        return flyway;
+    }
+
+    @Bean
+    PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
