33cd205e6e3e4d2bdbe4bbbff759ff992534f671
Trapp <nikolastrapp@gmail.com>
2025-03-27 21:57:27 -0300

    TEstes unitarios de HospedeServiceImpl

---
+++ src/main/java/br/com/trapp/cadastroagendabackend/util/Constantes.java
@@ -0,0 +1,9 @@
+package br.com.trapp.cadastroagendabackend.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constantes {
+
+    public static final String PACKAGE = "br.com.trapp.cadastroagendabackend";
+}
